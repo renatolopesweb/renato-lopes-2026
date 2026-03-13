7337799ffbbbd25d668ff38958f42aaef882c436
Renato Lopes <renato.ldeoliveira@gmail.com>
2026-03-13 18:03:33 -0300

    update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -4,7 +4,7 @@ const flagEs = document.querySelector('.flag-es');
 
 const descriptionSection = document.querySelector('.description-text');
 const descriptionPt = `Olá, sou o <strong class="text-blue">Renato Lopes</strong>, especialista na criação de websites usando tecnologias de CMS como Adobe Experience Manager (AEM), WordPress, Drupal e handcoded (HTML, CSS, JS). Também atuo com globalização de páginas web para projetos de pequena e grande escala.`;
-const descriptionEn = `Hello, I'm <strong class="text-blue">Renato Lopes</strong>, a specialist in website development using CMS technologies such as Adobe Experience Manager (AEM), WordPress, Drupal, and handcoded (HTML, CSS, JS). I also work with web page globalization, leading projects at both small and large scale.`;
+const descriptionEn = `Hello, I'm <strong class="text-blue">Renato Lopes</strong>, a specialist in website development using CMS technologies such as Adobe Experience Manager (AEM), WordPress, Drupal, and handcoded (HTML, CSS, JS). I also work with web page globalization for small and large scale.`;
 const descriptionEs = `Hola, soy <strong class="text-blue">Renato Lopes</strong>, especialista en la creación de sitios web usando tecnologías de CMS como Adobe Experience Manager (AEM), WordPress, Drupal y handcoded (HTML, CSS, JS). También trabajo con globalización de páginas web en pequeña y gran escala.`;
 
 const LangContact = document.querySelector('.language-contact');
