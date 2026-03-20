70347f28821f71d5e72cb302e1d2b5cc110d0648
Renato Lopes <renato.ldeoliveira@gmail.com>
2026-03-20 19:44:04 -0300

    update basic

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -3,14 +3,19 @@ const flagEn = document.querySelector('.flag-en');
 const flagEs = document.querySelector('.flag-es');
 
 const descriptionSection = document.querySelector('.description-text');
-const descriptionPt = `Olá, sou o <strong class="text-blue">Renato Lopes</strong>, especialista na criação de <strong>websites</strong> usando tecnologias de CMS como Adobe Experience Manager (AEM), WordPress ou handcoded (HTML, CSS, JS). Também atuo com globalização de páginas web para projetos de pequena e grande escala.`;
+const descriptionPt = `Olá, sou o <strong class="text-blue">Renato Lopes</strong>, especialista na criação de <strong>websites</strong> usando tecnologias de CMS como Adobe Experience Manager (AEM), WordPress, Drupal ou handcoded (HTML, CSS, JS). Também trabalho com globalização de páginas web para projetos de pequena e grande escala.`;
 const descriptionEn = `Hello, I'm <strong class="text-blue">Renato Lopes</strong>, a specialist in <strong>websites</strong>development using CMS technologies such as Adobe Experience Manager (AEM), WordPress, Drupal or handcoded (HTML, CSS, JS). I also work with web page globalization for small and large scale.`;
-const descriptionEs = `Hola, soy <strong class="text-blue">Renato Lopes</strong>, especialista en la creación de <strong>sitios web</strong> usando tecnologías de CMS como Adobe Experience Manager (AEM), WordPress, Drupal y handcoded (HTML, CSS, JS). También trabajo con globalización de páginas web en pequeña y gran escala.`;
+const descriptionEs = `Hola, soy <strong class="text-blue">Renato Lopes</strong>, especialista en la creación de <strong>sitios web</strong> usando tecnologías de CMS como Adobe Experience Manager (AEM), WordPress, Drupal o handcoded (HTML, CSS, JS). También trabajo con globalización de páginas web en pequeña y gran escala.`;
 
 const LangContact = document.querySelector('.language-contact');
-const LangContactPt = `Contato`;
-const LangContactEn = `Contact`;
-const LangContactEs = `Contato`;
+const LangContactPt = `Contato:`;
+const LangContactEn = `Contact:`;
+const LangContactEs = `Contato:`;
+
+const LangProject = document.querySelector('.language-project');
+const LangProjectPt = `Projetos selecionados:`;
+const LangProjectEn = `Selected projects:`;
+const LangProjectEs = `Proyectos seleccionados:`;
 
 const LangCta = document.querySelectorAll('.cta-access');
 const LangCtaPt = `Visitar Website`;
@@ -24,17 +29,20 @@ flagPt.addEventListener('click', (e) => {
   e.preventDefault();
   descriptionSection.innerHTML = descriptionPt;
   LangContact.innerHTML = LangContactPt;
+  LangProject.innerHTML = LangProjectPt;
   LangCta.forEach(el => el.innerHTML = LangCtaPt);
 });
 flagEn.addEventListener('click', (e) => {
   e.preventDefault();
   descriptionSection.innerHTML = descriptionEn;
   LangContact.innerHTML = LangContactEn;
+  LangProject.innerHTML = LangProjectEn;
   LangCta.forEach(el => el.innerHTML = LangCtaEn);
 });
 flagEs.addEventListener('click', (e) => {
   e.preventDefault();
   descriptionSection.innerHTML = descriptionEs;
   LangContact.innerHTML = LangContactEs;
+  LangProject.innerHTML = LangProjectEs;
   LangCta.forEach(el => el.innerHTML = LangCtaEs);
 });
