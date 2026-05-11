4dc0df16b371e5d88b65c003962a0c6b89ca13cc
Renato Lopes <renato.ldeoliveira@gmail.com>
2026-05-11 10:38:12 -0300

    update section

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -7,15 +7,15 @@ const descriptionPt = `Olá, sou o <strong class="text-blue">Renato Lopes</stron
 const descriptionEn = `Hello, I'm <strong class="text-blue">Renato Lopes</strong>, a specialist in <strong>websites</strong> development using CMS technologies such as Adobe Experience Manager (AEM), WordPress, Drupal or handcoded (HTML, CSS, JS).`;
 const descriptionEs = `Hola, soy <strong class="text-blue">Renato Lopes</strong>, especialista en la creación de <strong>sitios web</strong> usando tecnologías de CMS como Adobe Experience Manager (AEM), WordPress, Drupal o handcoded (HTML, CSS, JS).`;
 
-const LangContact = document.querySelector('.language-contact');
-const LangContactPt = `Contato:`;
-const LangContactEn = `Contact:`;
-const LangContactEs = `Contato:`;
+// const LangContact = document.querySelector('.language-contact');
+// const LangContactPt = `Contato:`;
+// const LangContactEn = `Contact:`;
+// const LangContactEs = `Contato:`;
 
 const LangProject = document.querySelector('.language-project');
-const LangProjectPt = `Projetos selecionados:`;
-const LangProjectEn = `Selected projects:`;
-const LangProjectEs = `Proyectos seleccionados:`;
+const LangProjectPt = `Projetos selecionados`;
+const LangProjectEn = `Selected projects`;
+const LangProjectEs = `Proyectos seleccionados`;
 
 const LangCta = document.querySelectorAll('.cta-access');
 const LangCtaPt = `Visitar Website`;
@@ -28,21 +28,38 @@ const LangCtaEs = `Visitar Sitio Web`;
 flagPt.addEventListener('click', (e) => {
   e.preventDefault();
   descriptionSection.innerHTML = descriptionPt;
-  LangContact.innerHTML = LangContactPt;
+  // LangContact.innerHTML = LangContactPt;
   LangProject.innerHTML = LangProjectPt;
   LangCta.forEach(el => el.innerHTML = LangCtaPt);
 });
 flagEn.addEventListener('click', (e) => {
   e.preventDefault();
   descriptionSection.innerHTML = descriptionEn;
-  LangContact.innerHTML = LangContactEn;
+  // LangContact.innerHTML = LangContactEn;
   LangProject.innerHTML = LangProjectEn;
   LangCta.forEach(el => el.innerHTML = LangCtaEn);
 });
 flagEs.addEventListener('click', (e) => {
   e.preventDefault();
   descriptionSection.innerHTML = descriptionEs;
-  LangContact.innerHTML = LangContactEs;
+  // LangContact.innerHTML = LangContactEs;
   LangProject.innerHTML = LangProjectEs;
   LangCta.forEach(el => el.innerHTML = LangCtaEs);
 });
+
+const trigger = document.querySelector('.js-open-projects');
+const projectsSection = document.querySelector('#all-projects');
+
+if (trigger && projectsSection) {
+  trigger.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    // mostra a section
+    projectsSection.classList.add('is-visible');
+
+    // scroll suave até ela
+    projectsSection.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+}
